e071a61425abb8f52744f4e9db7944267c05dbe3
Christopher Cardea <ccardea@gmail.com>
2026-05-29 14:57:54 -0400

    Add base url for deployment to github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://ccardea.github.io/',
+  base: '/github.io',
+  trailingSlash: 'ignore',
   output: 'static',
 })
